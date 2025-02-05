36860ffdd414edeacbe21d0866017271b9b79d61
NouhaAbdelkader <125505854+NouhaAbdelkader@users.noreply.github.com>
2025-02-05 14:32:20 +0100

    arrays

--- src/main/java/com/example/poc_pfe/PocPfeApplication.java
+++ src/main/java/com/example/poc_pfe/PocPfeApplication.java
@@ -9,5 +9,29 @@ public class PocPfeApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PocPfeApplication.class, args);
-    }
+
+        // ************************ arrays *************************** //
+
+        // copying arrays
+        String[] copyFrom = {
+                "Affogato", "Americano", "Cappuccino", "Corretto", "Cortado",
+                "Doppio", "Espresso", "Frappucino", "Freddo", "Lungo", "Macchiato",
+                "Marocchino", "Ristretto" };
+
+        String[] copyTo = new String[7];
+        ///////
+        System.arraycopy(copyFrom, 2, copyTo, 0, 7);
+        //////
+
+        for (String coffee : copyTo) {
+            System.out.print(coffee + " ");
+        }    }
+
+
+
+    // copying arrays without creationg second array
+
+    String[] copyFrom = { "Affogato", "Americano", "Cappuccino", "Corretto", "Cortado", "Doppio", "Espresso" };
+    String[] copyTo = java.util.Arrays.copyOfRange(copyFrom, 2, 9);
+
 }
