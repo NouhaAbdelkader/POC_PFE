76d88d42360bcfcde6f8ca7d4ec30ee6a075abb4
NouhaAbdelkader <125505854+NouhaAbdelkader@users.noreply.github.com>
2025-02-05 09:25:13 +0100

    first commit

---
+++ src/main/java/com/example/poc_pfe/PocPfeApplication.java
@@ -0,0 +1,14 @@
+package com.example.poc_pfe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocPfeApplication {
+//first commit
+
+    public static void main(String[] args) {
+        SpringApplication.run(PocPfeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/poc_pfe/PocPfeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.poc_pfe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocPfeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
