7782190b59f12eaa25a0e25cd78d8208ec923cb5
NouhaAbdelkader <125505854+NouhaAbdelkader@users.noreply.github.com>
2025-02-06 15:57:01 +0100

    Generics

---
+++ src/main/java/Interfaces/ITimeClient.java
@@ -0,0 +1,11 @@
+package Interfaces;
+
+import java.time.LocalDateTime;
+
+public interface ITimeClient {
+    void setTime(int hour, int minute, int second);
+    void setDate(int day, int month, int year);
+    void setDateAndTime(int day, int month, int year,
+                        int hour, int minute, int second);
+    LocalDateTime getLocalDateTime();
+}
